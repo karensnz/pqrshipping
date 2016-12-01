a0e336cc2fd93d3850d8fcba4b11cf0d73fe6a0b
Raghu Betina <raghu@firstdraft.com>
2016-12-01 22:21:30 +0000

    Whitelist columns in Pickup for ActiveAdmin

--- app/admin/pickup.rb
+++ app/admin/pickup.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Pickup do
 
+ permit_params :user_id, :day, :time, :package_type, :materials_needed, :package_destination, :image_upload, :special_instructions
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
